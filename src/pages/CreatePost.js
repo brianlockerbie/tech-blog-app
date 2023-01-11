a640fa49ef7f5900ea55fae0bebd28d5bdf3e14a
Brian <brian.lockerbie@gmail.com>
2023-01-11 15:03:37 -0800

    Added useState and navigate to createPost

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
 
-const CreatePost = () => {
+function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
+
+  const postCollectionRef = collection(db, "posts");
+  let navigate = useNavigate();
+
+  const createPost = async () => {
+    await addDoc(postCollectionRef, {
+      title, 
+      postText, 
+      author: {name: auth.currentUser.displayName, id: auth.currentUser.uid },
+    });
+    navigate("/");
+  };
+
   return ( 
     <div className="createPostPage"> 
       <div className="cpContainer">
         <h1>Create A Post</h1>
         <div className="inputGp"> 
           <label> Title:</label>
-          <input placeholder="Title..." />
+          <input 
+            placeholder="Title..." 
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }} 
+          />
         </div>
         <div className="inputGp">
           <label> Post:</label>
-          <textarea placeholder="Post..." />
+          <textarea 
+            placeholder="Post..."
+            onChange={(event) => {
+              setPostText(event.target.value);
+            }} 
+          />
         </div>
-        <button> Submit Post</button>
+        <button onClick={createPost}> Submit Post</button>
       </div>
     </div>
   );
 }
+
 
 export default CreatePost;
