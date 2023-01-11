4ef2c0ad5cb94e439bdbe6d2989f420a93589ec4
Brian <brian.lockerbie@gmail.com>
2023-01-10 23:09:18 -0800

    Added authentication

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,18 +2,17 @@ import React from "react";
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth"; 
 
-function Login() {
-
+function Login({ setIsAuth }) {
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
-      
-    })
-
-  }
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+    });
+  };
 
   return <div className="loginPage">
     <p>Sign In With Google to Continue</p>
-    <button className="login-with-google-btn">
+    <button className="login-with-google-btn" onClick={signInWithGoogle}>
       Sign in with Google
     </button>
   </div>
