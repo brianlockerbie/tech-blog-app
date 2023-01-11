24e3cf2bb95c350d5e2f45926f4ed6f216992ce6
Brian <brian.lockerbie@gmail.com>
2023-01-10 23:25:57 -0800

    added logout button

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
 import { useState } from "react";
-
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
+  
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear()
+      setIsAuth(false)
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/createpost"> Create Post </Link>
-        <Link to="/login"> Login </Link>
+        {!isAuth ? (
+           <Link to="/login"> Login </Link>
+        ) : (
+          <button onClick={signUserOut}> Log Out</button>
+        )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
